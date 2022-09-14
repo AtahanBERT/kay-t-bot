412983eda92a11c36bd14364e7b7d9ef8c96e1d2
AtahanBERT <76434430+AtahanBERT@users.noreply.github.com>
2022-09-14 22:55:40 +0200

    Add files via upload

---
+++ src/activities/userUpdate.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js')
+const client = global.client
+const settings = require('../configs/settings.json')
+const moment = require('moment');
+const config = require('../configs/config');
+ require('moment-duration-format')
+
+module.exports = async(oldUser, newUser) => { 
+
+  if (newUser.username !== oldUser.username) {
+
+    let isMemberFake = (Date.now() - newUser.createdTimestamp) < 7*24*60*60*1000;
+    let kanal = client.channels.cache.get(settings.tagLog)
+    let sunucu = client.guilds.cache.get(config.GuildID)
+    let yeniÜyeC = sunucu.members.cache.get(newUser.id)
+
+    if(newUser.bot) return
+
+    if(isMemberFake) {
+        await newUser.roles.set(settings.suspect)
+    } else {
+
+if (!oldUser.username.includes(config.Tag) && newUser.username.includes(config.Tag) && !yeniÜyeC.roles.cache.has(settings.familyRole)) {
+await yeniÜyeC.roles.add(settings.familyRole)
+
+kanal.send(new Discord.MessageEmbed().setTitle(sunucu.name, sunucu.iconURL({dynamic: true})).setDescription(`<@!${newUser.id}> \`${config.Tag}\` Tagımızı alarak <@&${settings.familyRole}> rolüne sahip oldu!`).setFooter(config.Footer))
+}
+
+if (oldUser.username.includes(config.Tag) && !newUser.username.includes(config.Tag) && yeniÜyeC.roles.cache.has(settings.familyRole)) {
+await yeniÜyeC.roles.remove(settings.familyRole)
+
+kanal.send(new Discord.MessageEmbed().setTitle(sunucu.name, sunucu.iconURL({dynamic: true})).setDescription(`<@!${newUser.id}> \`${config.Tag}\` Tagımızı çıkartarak <@&${settings.familyRole}> rolünü kaybetti!`).setFooter(config.Footer))
+}
+}
+}}
+
+module.exports.configurator = { name: "userUpdate" }
---
+++ src/configs/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    OwnerID: "429357746002067493", //BOT OWNER
+    Owners: ["429357746002067493","577541066203332619"], //SUNUCU OWNER
+    GuildID: "1009179302585180250", //SUNUCU ID
+    VoiceID: "1009179304518746194", //SES ODASI ID
+
+    Prefixs: ["."], //PREFIXLER
+
+    Tag: "☆", //SUNUCU TAGINIZ
+    SecondaryTag: "☆", //DOKUNMASANIZDA OLUR
+
+    Status: "dnd", // dnd - online - idle
+    Message: "Developed By Atahan", //BOT DURUMU
+    Footer: "Atahan" //setFooter
+}
